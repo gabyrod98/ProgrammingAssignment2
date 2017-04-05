b6622b3fa7ca661d992faf14dc62533bf3188e2c
gabyrod98 <gabyrod98@gmail.com>
2017-04-05 15:03:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,15 @@
 # this function creates a special "matrix" object that can cache its inverse
-makeCacheMatrix <- function(x= matrix()) {   m <- NULL  #Inizialization of x and m  
+makeCacheMatrix <- function(x= matrix()) {   
+m <- NULL  #Inizialization of x and m  
 set <- function(y) {    #set takes an argument named y     
-        x <<- y   #assign the input argument to the x object   
-        m <<- NULL #Assign the value of NULL to the m object in the parent environment    
+ x <<- y   #assign the input argument to the x object   
+ m <<- NULL #Assign the value of NULL to the m object in the parent environment    
 }  
 get <- function() x # R retrives x from the parent environment of makeCacheMatrix  
 setinverse <- function(solve) m <<- solve  
 getinverse <- function() m  
- list( set=set, get=get, setinverse=setinverse, getinverse=getinverse)  }
+ list( set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+ }
         
 # Returns a matrix that is the inverse of x, if it is in the memory, it will be "pulled" from there. 
 # if not... it will be calculated 
