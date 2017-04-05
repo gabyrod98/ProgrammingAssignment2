6635ddd2ebec80e2bf5b1670be3ffb4d244c3a71
gabyrod98 <gabyrod98@gmail.com>
2017-04-05 15:16:50 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,65 +1,34 @@
 
 
-# this
-function creates a special "matrix" object that can cache its inverse
-
+# this function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix
 <- function(x= matrix()) {
-
    m <- NULL 
 #Inizialization of x and m
-
   set <- function(y) {    #set takes an argument named y 
-
     x <<- y   #assign the input argument to the x object
-
-    m <<- NULL #Assign the value of NULL
-to the m object in the parent environment
+    m <<- NULL #Assign the value of NULLto the m object in the parent environment
 
   } 
-
-  get <- function() x # R retrives x from
-the parent environment of makeCacheMatrix
-
-  setinverse <- function(solve) m <<-
-solve
-
+  get <- function() x # R retrives x fromthe parent environment of makeCacheMatrix
+  setinverse <- function(solve) m <<- solve
   getinverse <- function() m
-
-  list( set=set, get=get, setinverse=setinverse,
-getinverse=getinverse)
+  list( set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
-# Returns a
-matrix that is the inverse of x, if it is in the memory, it will be
-"pulled" from there. 
-
-# if not...
-it will be calculated 
-
+# Returns a matrix that is the inverse of x, if it is in the memory, it will be "pulled" from there. 
+# if not...it will be calculated 
 cachesolve
 <- function(x,...){
-
   m <- x$getinverse()
-
-  if (!is.null(m)) { # this is done when the
-matrix is already in the memory
-
+  if (!is.null(m)) { # this is done when the matrix is already in the memory
     message("getting cached data")
-
     return(m)
-
-  }
-
+ }
   data <- x$get()
-
-  m <- solve(data,...) #this is done when
-the matrix is new data
-
+  m <- solve(data,...) #this is done when the matrix is new data
   x$setinverse(m)
-
   m
-
 }
 
